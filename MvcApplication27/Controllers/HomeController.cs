19d25957275a3dbb67a7dc05f4a077589eb731e6
Avrumi Friedman <info@lakewoodprogramming.com>
2016-06-02 12:34:43 -0400

    Initial commit

---
+++ MvcApplication27/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication27.Controllers
+{
+    public class RandomNumber
+    {
+        public int Number { get; set; }
+        public int[] MoreNums { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetRandom(int min, int max)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(min, max);
+            RandomNumber r = new RandomNumber();
+            int[] x = new int[rnd.Next(10, 20)];
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = rnd.Next(min, max);
+            }
+            r.Number = num;
+            r.MoreNums = x;
+            return Json(r, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult IsUsernameAvailable(string username)
+        {
+            return Json(new { isAvailable = username.Length == 5 }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Reversed(string text)
+        {
+            var obj = new { reversed = ReverseString(text) };
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+
+        private string ReverseString(string text)
+        {
+            return new String(text.Reverse().ToArray());
+        }
+
+        private string ReverseString2(string text)
+        {
+            string result = "";
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                result += text[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MvcApplication27/Scripts/index.js
@@ -0,0 +1,40 @@
+﻿//$(function() {
+//    $("#clickMe").on('click', function () {
+//        var min = $("#min").val();
+//        var max = $("#max").val();
+//        $.get('/home/getrandom?min=' + min + "&max=" + max, function (result) {
+//            $("#number").text(result.Number);
+//            result.MoreNums.forEach(function (num) {
+//                $("ul").append("<li>" + num + "</li>");
+//            });
+//        });
+//    });
+//});
+
+$(function() {
+    $("#clickMe").on('click', function () {
+        var minText = $("#min").val();
+        var maxText = $("#max").val();
+        $.post('/home/getrandom', { min: minText, max: maxText }, function (result) {
+            $("#number").text(result.Number);
+            result.MoreNums.forEach(function (num) {
+                $("ul").append("<li>" + num + "</li>");
+            });
+        });
+    });
+
+    $("#username").on('input', function() {
+        var text = $(this).val();
+        $.get('/home/isusernameavailable', { username: text }, function(result) {
+            $("#info").text(result.isAvailable ? "Go for it!" : "Sorry try again");
+            $("#submit-button").prop('disabled', !result.isAvailable);
+        });
+    });
+
+    $("#reverse-button").on('click', function() {
+        var text = $("#sometext").val();
+        $.get('/home/reversed', { text: text }, function(result) {
+            $("#reversed").text(result.reversed);
+        });
+    });
+});
